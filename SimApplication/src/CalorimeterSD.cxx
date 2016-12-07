f80d904600cf4b302814b32581663592f6ed2758
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-12-07 14:23:28 -0800

    updates 6_0_5

--- SimApplication/src/CalorimeterSD.cxx
+++ SimApplication/src/CalorimeterSD.cxx
@@ -4,12 +4,14 @@
 #include <iostream>
 
 // Geant4
+#include "G4Step.hh"
+#include "G4StepPoint.hh"
 #include "G4SDManager.hh"
+#include "G4Geantino.hh"
+#include "G4ChargedGeantino.hh"
 
 // LDMX
 #include "Event/RootEventWriter.h"
-
-using event::RootEventWriter;
 
 namespace sim {
 
