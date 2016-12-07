49667714c21dbef1501a00997a362cdc0fbe3b3d
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-12-06 21:00:22 -0800

    update_5_0_30

--- SimApplication/include/SimApplication/G4CalorimeterHit.h
+++ SimApplication/include/SimApplication/G4CalorimeterHit.h
@@ -70,9 +70,14 @@ class G4CalorimeterHit: public G4VHit {
             this->simCalHit_ = simCalHit;
         }
 
-        void ReadCalorimeterHit(ReadoutCalorimeterHit* readoutHit) {
+        void ReadCalorimeterHit(ReadoutCalorimeterHit* readoutHit, bool existingHit) {
 			readoutHit->setID(id_);
-			readoutHit->setEdep(edep_ + readoutHit->getEdep());
+			if(existingHit){
+				readoutHit->setEdep(edep_ + readoutHit->getEdep());
+			}
+			else{
+				readoutHit->setEdep(readoutHit->getEdep());
+			}
 			readoutHit->setPosition(position_.x(), position_.y(), position_.z());
 			readoutHit->setTime(time_);
             this->readCalHit_ = readoutHit;
