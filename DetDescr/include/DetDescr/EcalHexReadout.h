dfc6d81097f4a7730d12f41423f0c0ecd467692f
Owen Colegrove <ocolegro@Owens-MacBook-Pro.local>
2016-12-07 20:24:31 -0800

    up13

--- DetDescr/include/DetDescr/EcalHexReadout.h
+++ DetDescr/include/DetDescr/EcalHexReadout.h
@@ -26,7 +26,7 @@ class EcalHexReadout {
         inline std::pair<float,float> getCellCentroidXYPair(int cellId){
             std::pair<std::map<int,std::pair<float,float>>::iterator, bool> isInserted;
             if (!isInserted.second) {
-                std::cout << "This cellId does not exist, returning (-1,-1)" << std::endl
+                std::cout << "This cellId does not exist, returning (-1,-1)" << std::endl;
                 return std::make_pair<float,float>(-1.,-1.);
             }
             return isInserted.first->second;
